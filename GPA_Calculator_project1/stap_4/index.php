0852c2f4f18bb24863ae865878ed2f40434f9aa5
hanane-ber <hananeberrached12@gmail.com>
2026-04-19 02:37:21 +0200

    Update index.php

--- GPA_Calculator_project1/stap_4/index.php
+++ GPA_Calculator_project1/stap_4/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student GPA Calculator_project1 - Step 4</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <style>
         body { background-color: #f8f9fa; padding-top: 50px; }
         .container { background: white; padding: 30px; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1); }
